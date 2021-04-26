74202ec5ce338f749a8b5adb61b9ea5f4f1cdad9
Hritesh Sonawane <hriteshbebop@gmail.com>
2021-04-27 00:05:41 +0530

    first commit

---
+++ TypeMart/index.ts
@@ -0,0 +1,40 @@
+import products from "./products";
+
+let productName: string = "shirt";
+let shipping: number;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+const shippingAddress: string = "New York";
+
+const product = products.filter((product) => product.name === productName)[0];
+// console.log(product);
+
+// if (product.preOrder) {
+//   console.log("We will send you a message when your product is on its way.");
+// }
+
+if (Number(product.price) < 25) {
+  shipping = 0;
+  //   console.log("You get free shipping!");
+} else {
+  shipping = 5;
+}
+
+if (shippingAddress.match("New York")) {
+  taxPercent = 0.1;
+} else {
+  taxPercent = 0.05;
+}
+
+taxTotal = Number(product.price) * taxPercent;
+total = Number(product.price) + taxTotal + shipping;
+
+console.log(`
+Product:  ${product.name}
+Address:  ${shippingAddress}
+Price:    $${product.price}
+Tax:      $${taxTotal.toFixed(2)}
+Shipping: $${shipping.toFixed(2)}
+Total:    $${total.toFixed(2)}
+`);
